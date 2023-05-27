50836d49b0f76866d5708deaf62f9d377481bfce
Tiago Leite <contato@tiagocreator.com>
2023-05-27 15:09:19 -0300

    Add mark complete task and filter incomplete tasks

--- src/components/Focus.tsx
+++ src/components/Focus.tsx
@@ -2,9 +2,21 @@ import { TaskProps } from '../types';
 
 type Props = TaskProps & {};
 
-const Focus: React.FC<Props> = ({ tasks }) => {
-  const task = tasks[0];
-  return task ? <p>{task.label}</p> : <p></p>;
+const Focus: React.FC<Props> = ({ tasks, updateTask }) => {
+  const task = tasks.filter((task) => !task.completed)[0];
+
+  const handleCompletedTask = () => {
+    updateTask(task.id, true);
+  };
+
+  return task ? (
+    <div>
+      <p>{task.label}</p>
+      <button onClick={handleCompletedTask}>Completo</button>
+    </div>
+  ) : (
+    <p>Nenhuma tarefa incompleta.</p>
+  );
 };
 
 export default Focus;
