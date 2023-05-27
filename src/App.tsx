7229229dff971f2933b9c60dc8d1216332dc9f40
Tiago Leite <contato@tiagocreator.com>
2023-05-26 22:37:54 -0300

    Add nav links and focus component route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 
 import List from './components/List';
+import Focus from './components/Focus';
+
+const isActiveStyle = {
+  fontWeight: 'bold',
+};
 
 function App() {
   return (
     <BrowserRouter>
+      <nav>
+        <NavLink to='/' style={({ isActive }) => (isActive ? isActiveStyle : {})}>
+          Lista
+        </NavLink>
+        {' - '}
+        <NavLink to='/focus-task' style={({ isActive }) => (isActive ? isActiveStyle : {})}>
+          Foco
+        </NavLink>
+      </nav>
+      <br />
       <Routes>
         <Route path='/' element={<List />} />
-        <Route path='/focus-task'>Focar Tarefa</Route>
+        <Route path='/focus-task' element={<Focus />} />
       </Routes>
     </BrowserRouter>
   );
