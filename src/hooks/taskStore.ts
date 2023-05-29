b1d1062fb6b066d8e35c06a2dde19bd726b2f09b
Tiago Leite <contato@tiagocreator.com>
2023-05-28 21:42:38 -0300

    Create task store hook

---
+++ src/hooks/taskStore.ts
@@ -0,0 +1,42 @@
+import { useContext, useState } from 'react';
+import { shuffle } from 'lodash';
+import { nanoid } from 'nanoid';
+
+import { Task } from '../types';
+import TaskContext from '../context/taskStore';
+
+const useTaskStore = () => {
+  const [tasks, setTasks] = useContext(TaskContext);
+  const [focusedTaskId, setFocusedTaskId] = useState<string | undefined>(undefined);
+
+  const addTask = (task: Pick<Task, 'label'>) => {
+    const id = nanoid();
+    setTasks((tasks) => [...tasks, { label: task.label, id, completed: false }]);
+    if (!focusedTaskId) {
+      setFocusedTaskId(id);
+    }
+  };
+
+  const updateTask = (taskId: string, completed: boolean) => {
+    setTasks((tasks) =>
+      tasks.map((taskElement) => {
+        if (taskElement.id === taskId) {
+          return { ...taskElement, completed };
+        }
+        return taskElement;
+      }),
+    );
+  };
+
+  const focusedTask = tasks.find((task) => task.id === focusedTaskId);
+
+  const nextFocusedTask = () => {
+    setFocusedTaskId(shuffle(tasks.filter((task) => !task.completed))[0]?.id);
+  };
+
+  const api = { tasks, setTasks, addTask, updateTask, focusedTask, nextFocusedTask };
+
+  return api;
+};
+
+export default useTaskStore;
