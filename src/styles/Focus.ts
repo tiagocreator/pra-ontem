5984c34e3c8720fbeb57ceabadecdae5e2ae7520
Tiago Leite <contato@tiagocreator.com>
2023-06-01 09:34:34 -0300

    Create focus component styles

---
+++ src/styles/Focus.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+export const FocusTask = styled.div`
+  align-items: center;
+  display: flex;
+  flex: 1;
+  font-size: 32px;
+  justify-content: center;
+  padding-block-end: 45px;
+`;
