b16c5136e0e01a16f032febbaf9ef62a3319f4af
Tiago Leite <contato@tiagocreator.com>
2023-06-01 09:35:57 -0300

    Create main tag styles file

---
+++ src/styles/Main.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  padding: 35px;
+`;
